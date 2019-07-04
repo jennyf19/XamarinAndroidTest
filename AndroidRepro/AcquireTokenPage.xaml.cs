dc304bd4c14da0c86b0c7da395940f53b2f78403
Jenny Ferries <jeferrie@microsoft.com>
2019-07-03 19:24:37 -0700

    again

---
+++ AndroidRepro/AcquireTokenPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AndroidRepro
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AcquireTokenPage : ContentPage
+    {
+        public AcquireTokenPage()
+        {
+            InitializeComponent();
+        }
+        public PublicClientApplication PublicClientApplication { get; set; }
+        private const string EmptyResult = "Result:";
+
+        public void InitPublicClient()
+        {
+            var builder = PublicClientApplicationBuilder
+                .Create(App.ClientId)
+                .WithAuthority(new Uri(App.DefaultAuthority));
+
+                builder = builder.WithRedirectUri(App.s_redirectUriOnAndroid);
+
+                builder.Build();
+        }
+
+        private void OnPickerSelectedIndexChanged(object sender, EventArgs args)
+        {
+            var selectedTest = (Picker)sender;
+            int selectedIndex = selectedTest.SelectedIndex;
+
+            switch (selectedIndex)
+            {
+                case 0: // AT Interactive
+                    AcquireTokenInteractiveAsync(Prompt.ForceLogin).ConfigureAwait(false);
+                    break;
+            }
+        }
+
+        private async Task AcquireTokenInteractiveAsync(Prompt prompt)
+        {
+            try
+            {
+                AcquireTokenInteractiveParameterBuilder request = PublicClientApplication.AcquireTokenInteractive(App.s_defaultScopes)
+                    .WithPrompt(prompt)
+                    .WithParentActivityOrWindow(App.AndroidActivity)
+                    .WithUseEmbeddedWebView(true);
+
+                AuthenticationResult result = await
+                    request.ExecuteAsync().ConfigureAwait(true);
+
+                var resText = GetResultDescription(result);
+
+                acquireResponseLabel.Text = resText;
+            }
+            catch (Exception exception)
+            {
+               
+            }
+        }
+
+        private static string GetResultDescription(AuthenticationResult result)
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine("AccessToken : " + result.AccessToken);
+            sb.AppendLine("IdToken : " + result.IdToken);
+            sb.AppendLine("ExpiresOn : " + result.ExpiresOn);
+            sb.AppendLine("TenantId : " + result.TenantId);
+            sb.AppendLine("Scope : " + string.Join(",", result.Scopes));
+            sb.AppendLine("User :");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ AndroidRepro/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AndroidRepro
+{
+    public partial class App : Application
+    {
+        public static object AndroidActivity { get; set; }
+
+        public const string ClientId = "4b0db8c2-9f26-4417-8bde-3f0e3656f8e0";
+        public static string s_redirectUriOnAndroid = "urn:ietf:wg:oauth:2.0:oob";
+
+        public const string DefaultAuthority = "https://login.microsoftonline.com/common";
+        public static string[] s_defaultScopes = { "User.Read" };
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
